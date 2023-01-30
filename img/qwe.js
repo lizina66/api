5bfde6c3b181999541f4a5bb338f439dafe6df9e
栗 <121975413+lizina66@users.noreply.github.com>
2023-01-30 23:31:16 +0800

    Upload by PicGo

---
+++ img/qwe.js
@@ -0,0 +1,59 @@
+function copyUrl1()
+{
+var Url1=document.getElementById("QWE1").innerText;
+var oInput = document.createElement('input');
+oInput.value = Url1;
+document.body.appendChild(oInput);
+oInput.select(); // 选择对象
+document.execCommand("Copy"); // 执行浏览器复制命令
+oInput.className = 'oInput';
+oInput.style.display='none';
+}
+
+function copyUrl2()
+{
+var Url1=document.getElementById("QWE2").innerText;
+var oInput = document.createElement('input');
+oInput.value = Url1;
+document.body.appendChild(oInput);
+oInput.select(); // 选择对象
+document.execCommand("Copy"); // 执行浏览器复制命令
+oInput.className = 'oInput';
+oInput.style.display='none';
+}
+
+function copyUrl3()
+{
+var Url1=document.getElementById("QWE3").innerText;
+var oInput = document.createElement('input');
+oInput.value = Url1;
+document.body.appendChild(oInput);
+oInput.select(); // 选择对象
+document.execCommand("Copy"); // 执行浏览器复制命令
+oInput.className = 'oInput';
+oInput.style.display='none';
+}
+
+function copyUrl4()
+{
+var Url1=document.getElementById("QWE4").innerText;
+var oInput = document.createElement('input');
+oInput.value = Url1;
+document.body.appendChild(oInput);
+oInput.select(); // 选择对象
+document.execCommand("Copy"); // 执行浏览器复制命令
+oInput.className = 'oInput';
+oInput.style.display='none';
+}
+
+function copyUrl5()
+{
+var Url1=document.getElementById("QWE5").innerText;
+var oInput = document.createElement('input');
+oInput.value = Url1;
+document.body.appendChild(oInput);
+oInput.select(); // 选择对象
+document.execCommand("Copy"); // 执行浏览器复制命令
+oInput.className = 'oInput';
+oInput.style.display='none';
+}
